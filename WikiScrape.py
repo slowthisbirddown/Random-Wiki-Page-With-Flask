d4d9d159d0436492b729572e1ca4bf96177c3bb6
slowthisbirddown <hupo1989@gmail.com>
2020-10-26 20:15:36 -0600

    Imported bs4

--- WikiScrape.py
+++ WikiScrape.py
@@ -1,1 +1,12 @@
 import requests
+from bs4 import BeautifulSoup
+
+response = requests.get(
+    url="https://en.wikipedia.org/wiki/Web_scraping",
+
+)
+
+soup = BeautifulSoup(response.content, 'html.parser')
+
+title = soup.find(id="firstHeading")
+print(title.string)
